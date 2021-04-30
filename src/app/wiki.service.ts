3481995fedea70dbabd0e07ccb011df7676ff2a3
prasad <prasad@tech.in>
2021-04-30 16:47:40 +0530

    test

--- src/app/wiki.service.ts
+++ src/app/wiki.service.ts
@@ -1,9 +1,63 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WikiService {
+  constructor() {}
 
-  constructor() { }
+  getWikiLink() {
+    return WikiLink;
+  }
+
+  getWikiEnv() {
+    return WikiEnv;
+  }
 }
+
+const WikiLink = [
+  {
+    title: 'CI CD',
+    data: [
+      { name: 'Jenkins', link: 'asdfas/adsfa/asdf' },
+      { name: 'TeamCity', link: 'asdfas/adsfa/asdf' },
+    ],
+  },
+  {
+    title: 'CI CD 2',
+    data: [
+      { name: 'Jenkins', link: 'asdfas/adsfa/asdf' },
+      { name: 'TeamCity', link: 'asdfas/adsfa/asdf' },
+    ],
+  },
+];
+
+const WikiEnv = [
+  {
+    env: 'DEV',
+    title: 'abcd',
+    subTitle: 'asdfasd',
+    data: [
+      { name: 'aaa', link: 'asda' },
+      { name: 'aaa', link: 'asda' },
+    ],
+  },
+  {
+    env: 'QA',
+    title: 'abcd',
+    subTitle: 'asdfasd',
+    data: [
+      { name: 'aaa', link: 'asda' },
+      { name: 'aaa', link: 'asda' },
+    ],
+  },
+  {
+    env: 'Prod',
+    title: 'abcd',
+    subTitle: 'asdfasd',
+    data: [
+      { name: 'aaa', link: 'asda' },
+      { name: 'aaa', link: 'asda' },
+    ],
+  },
+];
