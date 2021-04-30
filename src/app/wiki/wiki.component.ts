8e96b8a2d93f065c70bc20688d2d0e090a366b52
SaiKumar9889 <48714889+SaiKumar9889@users.noreply.github.com>
2021-04-30 18:54:32 +0530

    up

--- src/app/wiki/wiki.component.ts
+++ src/app/wiki/wiki.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { WikiService } from '../wiki.service';
 
 @Component({
   selector: 'app-wiki',
   templateUrl: './wiki.component.html',
-  styleUrls: ['./wiki.component.scss']
+  styleUrls: ['./wiki.component.scss'],
 })
 export class WikiComponent implements OnInit {
+  @ViewChild('sidenav') sidenav: MatSidenav;
+  isExpanded = true;
+  showSubmenu: boolean = false;
+  isShowing = false;
+  showSubSubMenu: boolean = false;
 
-  constructor() { }
+  names: any[];
+  nameList: any[];
 
-  ngOnInit(): void {
+  constructor(private service: WikiService) {
+    this.names = service.getWikiLink();
+    this.nameList.push(this.names[0].name);
   }
 
+  ngOnInit(): void {}
 }
